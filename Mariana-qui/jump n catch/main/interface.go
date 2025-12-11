9e3a266fcbb5e75205b940e0b6b6f83589394ce8
marh12hm <shark07729@sharkcoders.pt>
2025-12-11 19:57:20 +0000

    Mariana
    Este é o meu projeto, pessoas.

---
+++ Mariana-qui/jump n catch/main/interface.go
@@ -0,0 +1,8 @@
+components {
+  id: "interface"
+  component: "/main/interface.gui"
+  position {
+    x: -0.4332389
+    y: 0.49821356
+  }
+}
---
+++ Mariana-qui/jump n catch/main/item.go
@@ -0,0 +1,39 @@
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"item\"\n"
+  "mask: \"player\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "      x: 1.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 4.7848687\n"
+  "}\n"
+  ""
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"moeda.png\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/assets/tiles/player.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: -1.0
+    y: -3.0
+  }
+}
